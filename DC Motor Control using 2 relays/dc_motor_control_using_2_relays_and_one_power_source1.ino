128b168236f7821556fbcc7992c73e8a08fe3a1f
MhdWaleedKoubeisi <53276079+MhdWaleedKoubeisi@users.noreply.github.com>
2020-07-10 05:24:34 +0300

    DC Motor Control using 2 relays

---
+++ DC Motor Control using 2 relays/dc_motor_control_using_2_relays_and_one_power_source1.ino
@@ -0,0 +1,15 @@
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+}
+
+void loop()
+{
+  	digitalWrite(13,HIGH);
+  	digitalWrite(12,LOW);
+  	delay(5000);
+  	digitalWrite(12,HIGH);
+  	digitalWrite(13,LOW);
+  	delay(5000);
+}
